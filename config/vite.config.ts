197faacf9cc95f43fc51a9cbeea07f71dda0044a
nodove <nodove@naver.com>
2025-08-26 22:39:45 +0900

    fix: vite base 경로를 /blog/로 설정 - GitHub Pages subpath MIME 타입 오류 해결

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -7,7 +7,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/blog/',
   publicDir: 'public',
   server: {
     host: '::',
